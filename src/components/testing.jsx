a39edb0ceff863335f3ba75397466d85ad91aba6
song <song@songui-Macmini.local>
2021-10-26 15:54:59 +0900

    20211026

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Testing = (props) => {
-    console.log(props);
-    return(<h1>hihihihihi</h1>);
+  console.log(props);
+  return <h1>hihihihihi</h1>;
 };
 
 export default Testing;
